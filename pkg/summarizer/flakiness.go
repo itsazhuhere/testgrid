4fa08e041dbb1852d70afe664e046a6fd0cb4983
Andre Zhu <andrezhu@google.com>
2020-06-16 10:45:34 -0700

    WIP changes to add flakiness report to summarizer
    Adds:
    - Parse state.proto Grid to array of structs with relevant data for a time span
    - Calculate naive flakiness similar to old flakiness report
    - Tests for most added functions

--- pkg/summarizer/flakiness.go
+++ pkg/summarizer/flakiness.go
@@ -2,40 +2,105 @@ package summarizer
 
 import (
 	"context"
+	"fmt"
+	"regexp"
+	"sort"
+	"strings"
 
 	"github.com/GoogleCloudPlatform/testgrid/internal/result"
 	"github.com/GoogleCloudPlatform/testgrid/pb/state"
+	"github.com/sirupsen/logrus"
+	"vbom.ml/util/sortorder"
 )
 
+var INFRA_REGEX = regexp.MustCompile(`^\w+$`)
+var JAILED_REGEX = regexp.MustCompile(`(JAILED )?(\/\/[a-z0-9\/:-_]+) - \[([a-z0-9\/:-_.]+)\]\s`)
+
+// Keep in mind that flakiness is measured as out of 100, i.e. 23 not .23
+var DEFAULT_FLAKINESS = 50.0
+var MIN_RUNS = 0
+var HEALTHY_RANGE = map[int]string{
+	20: "red",
+	10: "purple",
+	3:  "orange",
+	0:  "green",
+}
+
 type Result struct {
-	name          string
-	passed        int
-	failed        int
-	failedInfra   int
-	infraFailures int
+	name             string
+	passed           int
+	failed           int
+	flakyCount       int
+	averageFlakiness float64
+	failedInfraCount int
+	infraFailures    map[string]int
 }
 
-func AnalyzeFlakiness(grid *state.Grid, timeInterval int) {
-
+// Temporary structs while I decide what will go into summary.proto
+type Healthiness struct {
+	startDate        int
+	endDate          int
+	tests            []TestInfo
+	totalTests       int
+	totalJailedTests int
+	averageFlakiness float64
+	flakyBuckets     []FlakyBucket
+	infraIssues      map[string]int
+	totalConfigs     int
 }
 
-func ParseGrid(grid *state.Grid, startTime int, endTime int) {
+type TestInfo struct {
+	name               string
+	env                string
+	totalRuns          int
+	totalRunsWithInfra int
+	passedRuns         int
+	failedRuns         int
+	failedInfraRuns    int
+	flakyRuns          int
+	flakiness          float64
+	infraInfo          string
+}
+
+type FlakyBucket struct {
+	threshold float64
+	tests     int
+}
+
+type IntString struct {
+	s string
+	i int
+}
+
+// Definitions to allow for
+type IntStringSortable []IntString
+
+func (list IntStringSortable) Len() int      { return len(list) }
+func (list IntStringSortable) Swap(i, j int) { list[i], list[j] = list[j], list[i] }
+
+func (list IntStringSortable) Less(i, j int) bool {
+	if list[i].i < list[j].i {
+		return true
+	}
+	if list[i].i > list[j].i {
+		return false
+	}
+	return sortorder.NaturalLess(list[i].s, list[j].s)
+}
+
+func CalculateHealthiness(grid *state.Grid, startTime int, endTime int) Healthiness {
+	results := parseGrid(grid, startTime, endTime)
+	return analyzeFlakinessFromResults(results, startTime, endTime)
+}
+
+func parseGrid(grid *state.Grid, startTime int, endTime int) []Result {
 	// Get the relevant data for flakiness from each Grid (which represents
-	// a dashboard tab)
-
-	// grid.Columns will have column info, specifically timestamps
-	// we can filter timestamps first instead of doing it row by row
-	// can we assume that timestamps are in order?
-
-	// We know that result.Iter will act as an iterable, so we can avoid accessing the
-	// first half of the iterable twice by determining which indices will correspond to which
-	// time spans
-	// e.g [startCurrent to endCurrent], [endCurrent + 1 == startPrev to endPrev
+	// a dashboard tab) as a list of Result structs
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	results := make([]string, 0)
+	results := make([]Result, 0)
 
 	for _, test := range grid.Rows {
 		// each row in Rows will have a field results
@@ -43,28 +108,176 @@ func ParseGrid(grid *state.Grid, startTime int, endTime int) {
 		// e.g. (encoded -> decoded equivalent):
 		// [0, 3, 5, 4] -> [0, 0, 0, 5, 5, 5, 5]
 		// decoded int values correspond to Row.Result enum
-
-		// There's no guarantee that Grid.Columns is in order, although it most likely is in order
-		for i, testRun := range result.Map(ctx, test) {
-			if !IsWithinTimeFrame(grid.Columns[i], startTime, endTime) {
+		var resultCounts Result
+		resultCounts.infraFailures = make(map[string]int)
+		i := -1
+		for testResult := range result.Iter(ctx, test.Results) {
+			i += 1
+			if !isWithinTimeFrame(grid.Columns[i], startTime, endTime) {
 				continue
 			}
-			switch rowResult := result.Coalesce(test); rowResult {
+			switch rowResult := result.Coalesce(testResult, result.IgnoreRunning); rowResult {
+			case state.Row_NO_RESULT:
+				continue
 			case state.Row_FAIL:
-
+				categorizeFailure(&resultCounts, test.Messages[i])
+			case state.Row_PASS:
+				resultCounts.passed += 1
+			case state.Row_FLAKY:
+				getValueOfFlakyResult(&resultCounts)
 			}
 		}
-		// switch on test.status against status.TestStatus and increment Result.passed/failed based on value
+		if resultCounts.failed > 0 || resultCounts.passed > 0 || resultCounts.flakyCount > 0 {
+			resultCounts.name = test.Name
+			results = append(results, resultCounts)
+		}
 
 	}
+	return results
+}
+
+func analyzeFlakinessFromResults(results []Result, startTime int, endTime int) Healthiness {
+	// TODO: minRuns
+	return naiveFlakiness(results, MIN_RUNS, startTime, endTime)
+}
+
+func naiveFlakiness(results []Result, minRuns int, startDate int, endDate int) Healthiness {
+	testByEnv := make(map[string]TestInfo)
+	infraIssues := make(map[string]int)
+
+	for _, test := range results {
+		name := test.name
+		env := ""
+		match := JAILED_REGEX.FindStringSubmatch(name)
+		if match != nil {
+			name = match[1]
+			env = match[2]
+		} else {
+			logrus.Infof("Test with name \"%s\" could not be split into name and env", name)
+		}
+		if len(test.infraFailures) > 0 {
+			for errorType, errorCount := range test.infraFailures {
+				infraIssues[test.name+"-"+errorType] += errorCount
+			}
+		}
+
+		testInfo, success := calculateNaiveFlakiness(test, minRuns)
+		if !success {
+			continue
+		}
+		testInfo.name = name
+		testInfo.env = env
+
+		if currTestInfo, exists := testByEnv[name]; !exists || currTestInfo.flakiness < testInfo.flakiness {
+			testByEnv[name] = testInfo
+		}
+	}
+	// Populate Healthiness with above calculated information
+	healthiness := createHealthiness(startDate, endDate, results, testByEnv, infraIssues)
+	return healthiness
+}
+
+func createHealthiness(startDate int, endDate int, results []Result, testByEnv map[string]TestInfo, infraIssues map[string]int) Healthiness {
+	healthiness := Healthiness{
+		startDate:   startDate,
+		endDate:     endDate,
+		infraIssues: make(map[string]int),
+	}
+
+	// Go way of sorting the keys of a map in descending order
+	var keys []int
+	for k := range HEALTHY_RANGE {
+		keys = append(keys, k)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(keys)))
+	for threshold := range keys {
+		newBucket := FlakyBucket{
+			threshold: float64(threshold),
+		}
+		healthiness.flakyBuckets = append(healthiness.flakyBuckets, newBucket)
+	}
+
+	averageFlakiness := 0.0
+	for _, testInfo := range testByEnv {
+		healthiness.tests = append(healthiness.tests, testInfo)
+		averageFlakiness += testInfo.flakiness
+		for _, bucket := range healthiness.flakyBuckets {
+			if testInfo.flakiness > bucket.threshold {
+				bucket.tests += 1
+			}
+		}
+	}
+	healthiness.totalTests = len(healthiness.tests)
+	healthiness.totalConfigs = len(results)
+	if healthiness.totalTests > 0 {
+		healthiness.averageFlakiness = averageFlakiness / float64(healthiness.totalTests)
+	}
+	for k, v := range infraIssues {
+		healthiness.infraIssues[k] = v
+	}
+	return healthiness
+}
+
+func calculateNaiveFlakiness(test Result, minRuns int) (TestInfo, bool) {
+	failedCount := test.failed
+	totalCount := test.passed + test.failed
+	totalCountWithInfra := totalCount + test.failedInfraCount
+	if totalCount < minRuns {
+		return TestInfo{}, false
+	}
+	flakiness := 100 * float64(failedCount) / float64(totalCount)
+	infraInfo := calculateInfraInfo(test.infraFailures, test.failedInfraCount)
+	testInfo := TestInfo{
+		name:               "",
+		env:                "",
+		flakiness:          flakiness,
+		totalRuns:          totalCount,
+		totalRunsWithInfra: totalCountWithInfra,
+		passedRuns:         test.passed,
+		failedRuns:         test.failed,
+		failedInfraRuns:    test.failedInfraCount,
+		flakyRuns:          test.flakyCount,
+		infraInfo:          infraInfo,
+	}
+	return testInfo, true
 
 }
 
-func IsWithinTimeFrame(column *state.Column, startTime int, endTime int) bool {
-	// TODO: check what column.started represents (seconds or milliseconds)
+func calculateInfraInfo(issues map[string]int, failedCount int) string {
+	result := make([]string, 0)
+	if len(issues) > 0 && failedCount > 0 {
+		items := IntStringSortable{}
+		for key, value := range issues {
+			items = append(items, IntString{
+				s: key,
+				i: value,
+			})
+		}
+		sort.Sort(sort.Reverse(items))
+		for k, v := range issues {
+			result = append(result, k+fmt.Sprintf(" %.2f%% ", 100*float64(v)/float64(failedCount)))
+		}
+	}
+	return strings.TrimSpace(strings.Join(result, ""))
+}
+
+func categorizeFailure(resultCounts *Result, message string) {
+	if message == "" || !INFRA_REGEX.MatchString(message) {
+		resultCounts.failed += 1
+		return
+	}
+	resultCounts.failedInfraCount += 1
+	resultCounts.infraFailures[message] += 1
+}
+
+func getValueOfFlakyResult(resultCounts *Result) {
+	// Default behavior of adding a 50% flakiness
+	flakiness := DEFAULT_FLAKINESS
+	resultCounts.flakyCount += 1
+	// Formula for adding one new value to mean is mean + (newValue - mean) / newCount
+	resultCounts.averageFlakiness += (flakiness - resultCounts.averageFlakiness) / float64(resultCounts.flakyCount)
+}
+
+func isWithinTimeFrame(column *state.Column, startTime int, endTime int) bool {
 	return column.Started >= float64(startTime) && column.Started <= float64(endTime)
-}
-
-func ParseRow() {
-
 }
--- pkg/summarizer/flakiness_test.go
+++ pkg/summarizer/flakiness_test.go
@@ -1,14 +1,11 @@
 package summarizer
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/GoogleCloudPlatform/testgrid/pb/state"
 )
-
-func TestSomething(t *testing.T) {
-
-}
 
 func TestIsWithinTimeFrame(t *testing.T) {
 	cases := []struct {
@@ -58,9 +55,243 @@ func TestIsWithinTimeFrame(t *testing.T) {
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-
-			if actual := IsWithinTimeFrame(tc.column, tc.startTime, tc.endTime); actual != tc.expected {
+			if actual := isWithinTimeFrame(tc.column, tc.startTime, tc.endTime); actual != tc.expected {
 				t.Errorf("actual %t != expect %t", actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestParseGrid(t *testing.T) {
+	cases := []struct {
+		name      string
+		grid      *state.Grid
+		startTime int
+		endTime   int
+		expected  []Result
+	}{
+		{
+			name: "grid with all analyzed result types produces correct result list",
+			grid: &state.Grid{
+				Columns: []*state.Column{
+					{
+						Started: 0,
+					},
+					{
+						Started: 1,
+					},
+					{
+						Started: 2,
+					},
+					{
+						Started: 2,
+					},
+				},
+				Rows: []*state.Row{
+					{
+						Name: "test_1",
+						Results: []int32{
+							state.Row_Result_value["PASS"], 1,
+							state.Row_Result_value["FAIL"], 1,
+							state.Row_Result_value["FLAKY"], 1,
+							state.Row_Result_value["FAIL"], 1,
+						},
+						Messages: []string{
+							"",
+							"",
+							"",
+							"infra_fail_1",
+						},
+					},
+				},
+			},
+			startTime: 0,
+			endTime:   2,
+			expected: []Result{
+				{
+					name:             "test_1",
+					passed:           1,
+					failed:           1,
+					flakyCount:       1,
+					averageFlakiness: 50.0,
+					failedInfraCount: 1,
+					infraFailures: map[string]int{
+						"infra_fail_1": 1,
+					},
+				},
+			},
+		},
+		{
+			name: "grid with no analyzed results produces empty result list",
+			grid: &state.Grid{
+				Columns: []*state.Column{
+					{
+						Started: -1,
+					},
+					{
+						Started: 1,
+					},
+					{
+						Started: 2,
+					},
+					{
+						Started: 2,
+					},
+				},
+				Rows: []*state.Row{
+					{
+						Name: "test_1",
+						Results: []int32{
+							state.Row_Result_value["NO_RESULT"], 4,
+						},
+						Messages: []string{
+							"this message should not show up in results_0",
+							"this message should not show up in results_1",
+							"this message should not show up in results_2",
+							"this message should not show up in results_3",
+						},
+					},
+				},
+			},
+			startTime: 0,
+			endTime:   2,
+			expected:  []Result{},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			if actual := parseGrid(tc.grid, tc.startTime, tc.endTime); !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("\nactual %+v \n!= \nexpected %+v", actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestCreateHealthiness(t *testing.T) {
+	cases := []struct {
+		name        string
+		startDate   int
+		endDate     int
+		results     []Result
+		testByEnv   map[string]TestInfo
+		infraIssues map[string]int
+		expected    Healthiness
+	}{
+		{
+			name:        "typical inputs return correct Healthiness output",
+			startDate:   0,
+			endDate:     2,
+			results:     []Result{},
+			testByEnv:   map[string]TestInfo{},
+			infraIssues: map[string]int{},
+			expected: Healthiness{
+				startDate: 0,
+				endDate:   2,
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			if actual := createHealthiness(tc.startDate, tc.endDate, tc.results, tc.testByEnv, tc.infraIssues); !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("actual %+v != expected %+v", actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestCalculateNaiveFlakiness(t *testing.T) {
+	cases := []struct {
+		name             string
+		test             Result
+		minRuns          int
+		expectedTestInfo TestInfo
+		expectedSuccess  bool
+	}{
+		{
+			name:             "correctly filters Result with less than minRuns",
+			test:             Result{},
+			minRuns:          1000, // arbitrarily large number so that it should get filtered
+			expectedTestInfo: TestInfo{},
+			expectedSuccess:  false,
+		},
+		{
+			name: "typical Result returns correct TestInfo",
+			test: Result{
+				passed:           3,
+				failed:           2,
+				flakyCount:       8,
+				averageFlakiness: 0.5,
+				failedInfraCount: 4,
+				infraFailures: map[string]int{
+					"infra1": 3,
+					"infra2": 1,
+				},
+			},
+			minRuns: -1,
+			expectedTestInfo: TestInfo{
+				name:               "",
+				env:                "",
+				flakiness:          40.0,
+				totalRuns:          5,
+				totalRunsWithInfra: 9,
+				passedRuns:         3,
+				failedRuns:         2,
+				failedInfraRuns:    4,
+				flakyRuns:          8,
+				infraInfo:          "infra1 75.00% infra2 25.00%",
+			},
+			expectedSuccess: true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			if actualTest, actualSuccess := calculateNaiveFlakiness(tc.test, tc.minRuns); tc.expectedTestInfo != actualTest || tc.expectedSuccess != actualSuccess {
+				t.Errorf("\ntestInfo:\nactual: %v vs. expected: %v\nsuccess:\nactual: %v vs. expected: %v", actualTest, tc.expectedTestInfo, actualSuccess, tc.expectedSuccess)
+			}
+		})
+	}
+}
+
+func TestCalculateInfraInfo(t *testing.T) {
+	cases := []struct {
+		name        string
+		issues      map[string]int
+		failedCount int
+		expected    string
+	}{
+		{
+			name:        "empty issues map returns empty string",
+			issues:      map[string]int{},
+			failedCount: 10,
+			expected:    "",
+		},
+		{
+			name: "zero failedCount returns empty string",
+			issues: map[string]int{
+				"infra1": 2,
+				"infra2": 7,
+			},
+			failedCount: 0,
+			expected:    "",
+		},
+		{
+			name: "typical issues map returns properly formatted string",
+			issues: map[string]int{
+				"infra1": 2,
+				"infra2": 7,
+			},
+			failedCount: 10,
+			expected:    "infra2 70.00% infra1 20.00%",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			if actual := calculateInfraInfo(tc.issues, tc.failedCount); actual != tc.expected {
+				t.Errorf("actual: %s != exected: %s", actual, tc.expected)
 			}
 		})
 	}
